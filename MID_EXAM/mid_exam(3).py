74896de2bdfca97fa19dd1f3ae9840b3ee52ce5e
Bolba Mihai <anagageanu1@gmail.com>
2022-03-09 21:07:22 +0200

    exam

---
+++ MID_EXAM/mid_exam(3).py
@@ -0,0 +1,4 @@
+# Scrie un program care sa afiseze toate combinarile de 2 litere dintre valorile dictionarului de mai jos:
+#
+# dictionar = {"1": "abc","2": "s","3": "o","4": "n","5": "lm","6": "jk","7": "gi","8": "def","9": "abc"}
+
